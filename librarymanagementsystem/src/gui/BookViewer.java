99470c7c26b578a6f7ba851eebe61ca9ddad3042
Geonmo <rjsah8671@naver.com>
2023-06-03 14:21:11 +0900

    init

--- librarymanagementsystem/src/gui/BookViewer.java
+++ librarymanagementsystem/src/gui/BookViewer.java
@@ -1,32 +1,3 @@
-//package gui;
-//
-//import javax.swing.JFrame;
-//import javax.swing.JScrollPane;
-//import javax.swing.JTable;
-//import javax.swing.table.DefaultTableModel;
-//
-//public class BookViewer extends JFrame {
-//	
-//	public BookViewer() {
-//		DefaultTableModel model = new DefaultTableModel();
-//		model.addColumn("User Id");
-//		model.addColumn("Book Title");
-//		model.addColumn("Book Id");
-//		model.addColumn("Contact Info.");
-//
-//	String column[] = {"USERID", "TITLE", "BOOKID", "DATE."};
-//	
-//	JTable table = new JTable(new DefaultTableModel());
-//	JScrollPane sp = new JScrollPane(table);
-//	
-//	
-//	this.setSize(300, 300);
-//	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//	
-//	this.setVisible(true);
-//	}
-//}
-
 package gui;
 
 import javax.swing.JFrame;
@@ -43,10 +14,10 @@ public class BookViewer extends JFrame {
 		model.addColumn("Book Id");
 		model.addColumn("Contact Info.");
 
-		JTable table = new JTable(model); // 수정: 올바른 DefaultTableModel을 사용
+		JTable table = new JTable(model); 
 		JScrollPane sp = new JScrollPane(table);
 		
-		this.add(sp); // 수정: JScrollPane을 JFrame에 추가
+		this.add(sp); 
 		
 		this.setSize(300, 300);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);

42ce30bf1445a7f06f778fd6e0b4df55519f3b19
Geonmo <rjsah8671@naver.com>
2023-06-03 12:49:54 +0900

    init

--- librarymanagementsystem/src/gui/MenuSelection.java
+++ librarymanagementsystem/src/gui/MenuSelection.java
@@ -10,17 +10,16 @@ import javax.swing.JPanel;
 public class MenuSelection extends JFrame{
 
 	public MenuSelection() {
-		this.setSize(250, 300);
+		this.setSize(300, 300);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		JPanel panel1 = new JPanel();
 		JPanel panel2 = new JPanel();
 		JLabel label = new JLabel("Menu Selection");
 		
-		JButton button1 = new JButton("Add Book");
-		JButton button2 = new JButton("Add Book");
+		JButton button1 = new JButton("Checkout Book");
+		JButton button2 = new JButton("Return Book");
 		JButton button3 = new JButton("Edit Book");
-		JButton button4 = new JButton("Delete Book");
-		JButton button5 = new JButton("Exit Book");
+		JButton button4 = new JButton("Exit Book");
 		
 		
 		panel1.add(label);
@@ -28,7 +27,6 @@ public class MenuSelection extends JFrame{
 		panel2.add(button2);
 		panel2.add(button3);
 		panel2.add(button4);
-		panel2.add(button5);
 		
 		this.add(label, BorderLayout.NORTH);
 		this.add(panel2, BorderLayout.CENTER);
